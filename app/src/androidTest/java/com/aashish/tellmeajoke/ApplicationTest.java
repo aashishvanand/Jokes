552d39f7bdea252ab3415d0508566abc8a2117c9
Aashish Vivekanand <aashishvanand@gmail.com>
2016-09-03 16:35:59 +0530

    Design

--- app/src/androidTest/java/com/aashish/tellmeajoke/ApplicationTest.java
+++ app/src/androidTest/java/com/aashish/tellmeajoke/ApplicationTest.java
@@ -3,11 +3,24 @@ package com.aashish.tellmeajoke;
 import android.app.Application;
 import android.test.ApplicationTestCase;
 
+import java.util.concurrent.ExecutionException;
+
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
+    }
+    public void testFetchJokeTask() throws ExecutionException, InterruptedException {
+        GCEAsync testJoke = new GCEAsync(new GCEAsync.Callback() {
+            @Override
+            public void onFinished(String result) {
+
+            }
+        });
+        testJoke.execute();
+        String joke = testJoke.get();
+        assertNotNull(joke);
     }
 }
