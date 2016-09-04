c74bf0992a39c848b0c35dd68ce6820ac28c70a3
Aashish Vivekanand <aashishvanand@gmail.com>
2016-09-04 15:54:51 +0530

    Final

--- app/src/paid/java/com/aashish/tellmeajoke/MainActivity.java
+++ app/src/paid/java/com/aashish/tellmeajoke/MainActivity.java
@@ -1,5 +1,6 @@
 package com.aashish.tellmeajoke;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -10,15 +11,23 @@ import com.aashish.jokedisplay.DisplayActivity;
 public class MainActivity extends AppCompatActivity implements GCEAsync.Callback  {
 
     Button Tellajoke;
+    private ProgressDialog pDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Progress dialog
+        pDialog = new ProgressDialog(this);
+        pDialog.setCancelable(false);
+
+
         Tellajoke = (Button) findViewById(R.id.button_joke);
         Tellajoke.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
+                pDialog.setMessage("Loading");
+                showDialog();
                 getFetchJoke();
         }
         }
@@ -30,14 +39,24 @@ public class MainActivity extends AppCompatActivity implements GCEAsync.Callback
         new GCEAsync(this).execute();
     }
 
+    private void showDialog() {
+        if (!pDialog.isShowing())
+            pDialog.show();
+    }
+
+    private void hideDialog() {
+        if (pDialog.isShowing())
+            pDialog.dismiss();
+    }
+
 
     @Override
     public void onFinished(String result) {
-        //if failed to connect to server load from internal library
-            Intent intent = new Intent(MainActivity.this, DisplayActivity.class);
-            intent.putExtra("Joke", result);
-            String product = "Paid";
-            intent.putExtra("Product", product);
-            startActivity(intent);
+        hideDialog();
+        Intent intent = new Intent(MainActivity.this, DisplayActivity.class);
+        intent.putExtra("Joke", result);
+        String product = "Paid";
+        intent.putExtra("Product", product);
+        startActivity(intent);
     }
 }
